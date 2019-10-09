7d1f54b7bc6e853407b732d05ab6387e5cd8a4df
Alex <alexgilbert909@gmail.com>
2019-10-09 13:15:20 +0100

    First commit

---
+++ src/comps/custom-btn/customBtnStyles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const BtnContainer = styled.div`
+  background: #f0f7fe;
+  padding 0.75rem;
+  margin: 0 0.25rem;
+  border-radius: 5px;
+`;
+
+export const StyledLink = styled.a`
+  color: #729dc7;
+  font-size: 12px;
+  padding 0.75rem;
+`;
